a1e0f7dadd507481e3670798df73198ffe2b53b0
Ayboss <bamiayo90@gmail.com>
2020-12-23 20:50:34 +0100

    let us see what d problem is gan gan

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -56,7 +56,7 @@ const createBooking = catchError(async (session)=>{
 exports.webhookCheckout = catchError(async (req,res,next)=>{
     //get the body
     const signature = req.headers['stripe-signature'];
-    console.log(signature, req.body)
+    console.log('signature',signature,'body from weebhook', req.body)
     let event;
     try {
         event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
